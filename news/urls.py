90f9d91ca9bd29874b73c14f48e1051c6a5ec019
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-21 17:34:59 +0300

    Configure our news app

--- news/urls.py
+++ news/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
   path('', views.welcome, name='News~Welcome'),
   path('about/', views.about, name='News~About'),
   path('today/', views.news_of_day, name='News~Today'),
+  
+  # url path not working as expected
   path('archives/(\d{4}-\d{2}-\d{2})', views.past_days_news, name='News~Archives'),
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
 
 
@@ -48,10 +48,16 @@ def convert_dates(dates) :
   return day
 
 
+# Function not working
 def past_days_news(request, past_date) :
 
+  try :
   # Converts data from the string Url
-  date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+  except ValueError :
+  # Raise 404 error when ValueError is thrown
+    raise Http404()
 
   day= convert_dates(date)
 
