798caeec08a27e5c3ce276e09d01374269a6e483
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-21 15:02:05 +0300

    Add a function to catch archived news of a past date

--- news/urls.py
+++ news/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
   path('', views.welcome, name='News~Welcome'),
   path('about/', views.about, name='News~About'),
   path('today/', views.news_of_day, name='News~Today'),
+  path('archives/(\d{4}-\d{2}-\d{2})', views.past_days_news, name='News~Archives'),
 
 ]
