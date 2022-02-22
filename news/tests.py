b245c40b537f46c2cb54cfbd4dc7e160aad25957
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-22 19:24:07 +0300

    Test that there is an instance of the Editor model

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Article, Tag, Editor
+
+
 
 # Create your tests here.
+class EditorTestClass(TestCase) :
+  
+  #set up method
+  def setUp(self) :
+    self.josphine = Editor(first_name='Josphine', last_name ='Mbaisi', email ='mbaisijosphine@gmail.com')
+
+  # testing the instance
+  def test_instance(self) :
+    self.assertTrue(isinstance(self.josphine, Editor))
