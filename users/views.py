c1390b8d277c294593af463446fb2e9c546981d2
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-28 17:55:02 +0300

    Create the success message for registration and loop it through the pages in base.html file

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 
@@ -12,8 +13,21 @@ def register(request) :
     if form.is_valid() :
       username = form.cleaned_data.get('username')
 
+      messages.success(request, f'{ username }, your account has successfully been created!')
+
+      return redirect('Article')
+
   else :
   
     form = UserCreationForm()
 
   return render(request, 'users/register.html', { 'form':form })
+
+
+
+# Types of messages
+# messages.info
+# messages.success
+# messages.warning
+# messages.error
+# messages.debug
