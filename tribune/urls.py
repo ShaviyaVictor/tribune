46803bdfe697d75f534e015bec200ab6fa05d5a5
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-01 13:12:14 +0300

    Finally implemented the profile picture display feature

--- tribune/urls.py
+++ tribune/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from tribune.settings import DEBUG
 # from django.conf.urls import url
 from users import views as user_views
+
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib.auth import views as auth_views
 
@@ -34,3 +39,8 @@ urlpatterns = [
 
     re_path('profile/', user_views.profile, name='profile'),
 ]
+
+
+if settings.DEBUG :
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
