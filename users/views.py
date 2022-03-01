9e40df430de3f26e8ab887e19b30dae42dbac7d8
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-01 16:22:08 +0300

    Customize our forms to collect information for the profile

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 from django.contrib.auth.decorators import login_required
 
@@ -42,4 +42,12 @@ def register(request) :
 @login_required
 def profile(request) :
 
-  return render(request, 'users/profile.html')
+  u_form = UserUpdateForm()
+  p_form = ProfileUpdateForm()
+
+  context = {
+    'u_form': u_form,
+    'p_form': p_form,
+  }
+
+  return render(request, 'users/profile.html', context)
