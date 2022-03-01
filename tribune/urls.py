71af6dac4e3999d110648bd2f642920615b1bcdd
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-01 06:23:40 +0300

    Modify the path of the url path for login and logout

--- tribune/urls.py
+++ tribune/urls.py
@@ -18,12 +18,17 @@ from django.urls import path, include, re_path
 # from django.conf.urls import url
 from users import views as user_views
 
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     re_path('register/', user_views.register, name='register'),
-    # url(r'^register/', include('registration.backends.simple.urls')),
+
+    re_path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    re_path('logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
 
     path('', include('news.urls')),
 ]
