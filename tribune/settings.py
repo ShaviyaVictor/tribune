13ae3e504fc3b993610ea07fd40e2c96d027eaa1
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-01 12:34:53 +0300

    Redirect the profile pic folder to the images folder system of our app

--- tribune/settings.py
+++ tribune/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# MEDIA_URL = '/images/'
+MEDIA_URL = '/images/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
